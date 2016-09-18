dbda3e312b9137e5626ec11a904f4f92a5188346
hville <hugov@runbox.com>
2016-09-18 09:09:07 +0800

    kahan sums

--- index.js
+++ index.js
@@ -1,113 +1,61 @@
-'use strict'
-
-/**
- * sum of all array values
- * @private
- * @param   {Array}  arr Samples
- * @returns {Number} sum
- */
-function sum (arr) {
-  for (var i = 0, t = 0; i < arr.length; i++) t += arr[i]
-  return t
-}
-
-/**
- * sum of all array squares
- * @private
- * @param   {Array}  arr Samples
- * @returns {Number} sum
- */
-
-function sum2 (arr) {
-  for (var i = 0, t = 0; i < arr.length; i++) {
-    t += arr[i] * arr[i]
-  }
-  return t
-}
-
-/**
- * sum of all array values raised to a given exponent
- * @private
- * @param   {Array}  arr Samples
- * @param   {Number} exp exponent
- * @returns {Number} sum
- */
-function sumN (arr, exp) {
-  if (exp === 1) return sum(arr)
-  if (exp === 2) return sum2(arr)
-  for (var i = 0, t = 0; i < arr.length; i++) t += Math.pow(arr[i], exp)
-  return t
-}
-
-/**
- * sums of array value raised to exponents 1 and 2
- * @private
- * @param   {Array} arr Samples
- * @param   {Array} tgt resultArray
- * @returns {Array} resultArray
- */
-function sums2 (arr, tgt) {
-  for (var i = 0, t1 = 0, t2 = 0; i < arr.length; i++) {
-    t1 += arr[i]
-    t2 += arr[i] * arr[i]
-  }
-  tgt[0] = t1
-  tgt[1] = t2
-  return tgt
-}
-
-/**
- * sums of array values raised to increasing exponents
- * @private
- * @param   {Array} arr Samples
- * @param   {Array} tgt resultArray
- * @returns {Array} resultArray
- */
-function sumsN (arr, tgt) {
-  // faster functions for the simple cases
-  if (tgt.length === 0) return tgt
-  if (tgt.length === 1) {
-    tgt[0] = sum(arr)
-    return tgt
-  }
-  if (tgt.length === 2) return sums2(arr, tgt)
-
-  // init with first sample
-  var smp = arr[0]
-  var pro = smp
-  tgt[0] = pro
-  for (var j = 1; j < tgt.length; j++) {
-    pro *= smp
-    tgt[j] = pro
-  }
-  // add the rest
-  for (var i = 1; i < arr.length; i++) {
-    smp = arr[i]
-    pro = smp
-    tgt[0] += pro
-    for (j = 1; j < tgt.length; j++) {
-      pro *= smp
-      tgt[j] += pro
-    }
-  }
-  return tgt
-}
+module.exports = sumPower
 
 /**
  * sum or sums of values raised to one or more exponent
- * @param   {Array}  arr Samples
- * @param   {Number|Array} [N=1]   exponent or target array with length of max exponent
+ * @param	 {Array}	arr Samples
+ * @param	 {Number|Array} [N=1]	 exponent or target array with length of max exponent
  * @returns {Number} sum
  */
-function sumPower (arr, N) {
-  // if N is a number, return a single sum x^N
-  if (typeof N === 'number') return sumN(arr, N)
-
-  // is N is an array, return an array of sums [X^1, x^2, ..., x^length]
-  if (Array.isArray(N)) return sumsN(arr, N)
-
-  // default N=1
-  return sum(arr)
+function sumPower (arr, pow) {
+	if (pow === undefined) return sum1(arr)
+	if (pow.constructor === Number) return sumN(arr, pow)
+	if (Array.isArray(pow)) {
+		for (var i = 0; i < pow.length; ++i) pow[i] = sumN(arr, i+1)
+		return pow
+	}
+	throw Error('sumPower called with invalid arguments types')
 }
-
-module.exports = sumPower
+/**
+ * sum of all array values with some error correction (modified Kahan sum)
+ * @param	 {Array} arr Samples
+ * @returns {Number} ∑(a[i])
+ */
+function sum1(arr) {
+	for (var i=0, sum=0, err=0, tot=0; i<arr.length; ++i) {
+		sum += arr[i]
+		err += (sum-tot) - arr[i]
+		tot = sum
+	}
+	return sum - err
+}
+/**
+ * sum of all array values^2 with some error correction (modified Kahan sum)
+ * @param	 {Array} arr Samples
+ * @returns {Number} ∑(a[i]^2)
+ */
+function sum2(arr) {
+	for (var i=0, sum=0, err=0, tot=0; i<arr.length; ++i) {
+		var val = arr[i]*arr[i]
+		sum += val
+		err += (sum-tot) - val
+		tot = sum
+	}
+	return sum - err
+}
+/**
+ * sum of all array values^n with some error correction (modified Kahan sum)
+ * @param	 {Array} arr Samples
+ *  @param	 {Array} pow Power
+ * @returns {Number} ∑(a[i]^n)
+ */
+function sumN(arr, pow) {
+	if (pow === 1) return sum1(arr)
+	if (pow === 2) return sum2(arr)
+	for (var i=0, sum=0, err=0, tot=0; i<arr.length; ++i) {
+		var val = Math.pow(arr[i], pow)
+		sum += val
+		err += (sum-tot) - val
+		tot = sum
+	}
+	return sum - err
+}
--- test.js
+++ test.js
@@ -1,30 +1,23 @@
 'use strict'
-var test = require('tt')
+var c = require('cotest')
 var powerSums = require('./index.js')
 
-test('single sum', function (t) {
-  t.comment('normal use')
-  t.equal(powerSums([-1, -2, -3], 2), 14, 'sum square valid')
-  t.equal(powerSums([-1, -2, -3], 3), -36, 'sum cube valid')
-  t.equal(powerSums([0.5, 0.5, 0.5], -1), 6, 'negative exponents are valid')
-
-  t.comment('edge cases')
-  t.equal(powerSums([], 3), 0, 'empty samples have a sum of 0')
-  t.equal(powerSums([1, 2, 3]), 6, 'handles missing exponent - default exponent is 1')
-  t.ok(isNaN(powerSums([null, 'a', undefined], 1)), 'invalid samples should return NaN as sum')
-
-  t.end()
+c('single sum - normal use', function () {
+	c('===', powerSums([-1, -2, -3], 2), 14, 'sum square valid')
+	c('===', powerSums([-1, -2, -3], 3), -36, 'sum cube valid')
+	c('===', powerSums([0.5, 0.5, 0.5], -1), 6, 'negative exponents are valid')
 })
-
-test('multiple sums', function (t) {
-  t.comment('normal use')
-  t.deepEqual(powerSums([-1, -2, -3], Array(2)), [-6, 14], 'sum square valid')
-  t.deepEqual(powerSums([-1, -2, -3], Array(3)), [-6, 14, -36], 'sum cubes valid')
-
-  t.comment('edge cases')
-  t.deepEqual(powerSums([], Array(2)), [0, 0], 'empty samples have a sum of 0')
-  t.deepEqual(powerSums([0, 1, 2], Array(0)), [], 'empty parameter should yield empty results')
-  t.ok(isNaN(powerSums([null, 'a', undefined], Array(2))[1]), 'invalid samples should return NaN array')
-
-  t.end()
+c('single sum - edge cases', function () {
+	c('===', powerSums([], 3), 0, 'empty samples have a sum of 0')
+	c('===', powerSums([1, 2, 3]), 6, 'handles missing exponent - default exponent is 1')
+	c('===', isNaN(powerSums([null, 'a', undefined], 1)), true, 'invalid samples should return NaN as sum')
+})
+c('multiple sums - normal use', function () {
+	c('{==}', powerSums([-1, -2, -3], Array(2)), [-6, 14], 'sum square valid')
+	c('{==}', powerSums([-1, -2, -3], Array(3)), [-6, 14, -36], 'sum cubes valid')
+})
+c('multiple sums - edge cases', function () {
+	c('{==}', powerSums([], Array(2)), [0, 0], 'empty samples have a sum of 0')
+	c('{==}', powerSums([0, 1, 2], Array(0)), [], 'empty parameter should yield empty results')
+	c('===', isNaN(powerSums([null, 'a', undefined], Array(2))[1]), true, 'invalid samples should return NaN array')
 })
